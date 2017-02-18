e5e2f4f8aa1f0e5ce60268da9355f75a70777daa
Brian Hudson <hudson@ainfosec.com>
2017-02-18 07:13:44 -0500

    Add area chart example

---
+++ kubed-demos/src/main/kotlin/kubed/demo/AreaDemo.kt
@@ -0,0 +1,74 @@
+package kubed.demo
+
+import javafx.application.Application
+import javafx.geometry.Insets
+import javafx.scene.Group
+import javafx.scene.Node
+import javafx.scene.Scene
+import javafx.scene.paint.Color
+import javafx.stage.Stage
+import kubed.axis.axisBottom
+import kubed.axis.axisLeft
+import kubed.interpolate.interpolateRound
+import kubed.scale.LinearScale
+import kubed.scale.scaleBand
+import kubed.selection.selectAll
+import kubed.shape.area
+import kubed.shape.curve.*
+import kubed.shape.rect
+
+class AreaDemo: Application() {
+    override fun start(primaryStage: Stage?) {
+        val margin = Insets(20.0, 10.0, 20.0, 10.0)
+
+        val outerWidth = 960.0
+        val outerHeight = 500.0
+        val innerWidth = outerWidth - margin.left - margin.right
+        val innerHeight = outerHeight - margin.top - margin.bottom
+
+        val root = Group()
+        root.prefWidth(outerWidth)
+        root.prefHeight(outerHeight)
+        root.translateX = margin.left + 30.0
+        root.translateY = margin.top
+
+        val data = listOf(4, 2, 6, 3, 3, 7, 9, 2, 1, 6)
+
+        val xScale = LinearScale<Double>(::interpolateRound).range(listOf(0.0, innerWidth))
+                .domain(listOf(0.0, data.size - 1.0))
+
+        val yScale = LinearScale<Double>(::interpolateRound).range(listOf(innerHeight, 0.0))
+                .domain(listOf(0.0, 10.0))
+
+        val xAxis = axisBottom(xScale)
+        xAxis(root.selectAll(".xAxis").append(fun(): Node { return Group() })
+                .classed("axis", "xAxis")
+                .translateY(innerHeight))
+
+        val yAxis = axisLeft(yScale)
+        yAxis(root.selectAll(".yAxis").append(fun(): Node { return Group() })
+                .classed("axis", "yAxis"))
+
+        val area = area<Int> {
+            x { _, i, _ -> xScale(i.toDouble()) }
+            y1 { d, _, _ -> yScale(d.toDouble()) }
+            y0(yScale(0.0))
+            curve(curveCardinal())
+            fill(Color.STEELBLUE)
+        }
+
+        root.children += area(data)
+
+
+        val scene = Scene(root)
+        primaryStage?.scene = scene
+        primaryStage?.show()
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(vararg args: String) {
+            launch(AreaDemo::class.java, *args)
+        }
+    }
+}
