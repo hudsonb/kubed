1dc2b86e499684854c34029b3fefee5f530d0d10
Brian Hudson <hudson@ainfosec.com>
2017-02-18 07:17:52 -0500

    Add support for axis DSL syntax

--- kubed-core/src/main/kotlin/kubed/axis/package.kt
+++ kubed-core/src/main/kotlin/kubed/axis/package.kt
@@ -4,6 +4,13 @@ import javafx.geometry.Side
 import kubed.scale.Scale
 
 fun <D, R : Number> axisTop(scale: Scale<D, R>) = Axis(Side.TOP, scale)
+fun <D, R : Number> axisTop(scale: Scale<D, R>, init: Axis<D, R>.() -> Unit) = axisTop(scale).apply(init)
+
 fun <D, R : Number> axisRight(scale: Scale<D, R>) = Axis(Side.RIGHT, scale)
+fun <D, R : Number> axisRight(scale: Scale<D, R>, init: Axis<D, R>.() -> Unit) = axisRight(scale).apply(init)
+
 fun <D, R : Number> axisBottom(scale: Scale<D, R>) = Axis(Side.BOTTOM, scale)
+fun <D, R : Number> axisBottom(scale: Scale<D, R>, init: Axis<D, R>.() -> Unit) = axisBottom(scale).apply(init)
+
 fun <D, R : Number> axisLeft(scale: Scale<D, R>) = Axis(Side.LEFT, scale)
+fun <D, R : Number> axisLeft(scale: Scale<D, R>, init: Axis<D, R>.() -> Unit) = axisLeft(scale).apply(init)
