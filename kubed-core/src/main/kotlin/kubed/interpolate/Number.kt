11317a7532276c115be7b920c47664b42c560411
Brian Hudson <hudson@ainfosec.com>
2017-02-18 07:15:21 -0500

    Add interpolateRound

--- kubed-core/src/main/kotlin/kubed/interpolate/Number.kt
+++ kubed-core/src/main/kotlin/kubed/interpolate/Number.kt
@@ -4,3 +4,8 @@ fun interpolateNumber(a: Double, b: Double): (Double) -> Double {
     val d = b - a
     return { t -> a + d * t }
 }
+
+fun interpolateRound(a: Double, b: Double): (Double) -> Double {
+    val d = b - a
+    return { t -> Math.round(a + d * t).toDouble() }
+}
