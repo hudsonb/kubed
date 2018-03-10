5f83c361f1b72252fda5c613273cace6300c65b7
Brian Hudson <hudson@ainfosec.com>
2018-03-10 10:25:22 -0500

    Add Collignon projection

---
+++ kubed-geo/src/main/kotlin/kubed/geo/projection/Collignon.kt
@@ -0,0 +1,30 @@
+package kubed.geo.projection
+
+import kubed.geo.Position
+import kubed.geo.math.SQRT_PI
+import kubed.geo.math.asin
+import kotlin.math.PI
+import kotlin.math.sin
+import kotlin.math.sqrt
+
+fun collignon() = collignon {}
+fun collignon(init: MutableProjection.() -> Unit) = projection(CollignonProjector()) {
+    scale = 95.6464
+    center = Position(0.0, 30.0)
+    init()
+}
+
+class CollignonProjector : InvertableProjector {
+    override fun invoke(lambda: Double, phi: Double): DoubleArray {
+        val alpha = sqrt(1 - sin(phi))
+        return doubleArrayOf((2 / SQRT_PI) * lambda * alpha, SQRT_PI * (1 - alpha))
+    }
+
+    override fun invert(x: Double, y: Double): DoubleArray {
+        var lambda = y / SQRT_PI - 1
+        lambda *= lambda
+
+        return doubleArrayOf(if(lambda > 0) x * sqrt(PI / lambda) / 2 else 0.0,
+                             asin(1 - lambda))
+    }
+}
