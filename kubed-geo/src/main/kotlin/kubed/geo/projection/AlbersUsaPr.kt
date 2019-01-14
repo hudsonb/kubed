7e6cc8f68fb52ddf0cf5cacbb6247a94bec94985
Brian Hudson <brian.r.hudson@gmail.com>
2019-01-14 13:53:08 -0500

    Add AlbersUsaPr (usa + puerto rico) projection

---
+++ kubed-geo/src/main/kotlin/kubed/geo/projection/AlbersUsaPr.kt
@@ -0,0 +1,172 @@
+package kubed.geo.projection
+
+import javafx.geometry.Rectangle2D
+import kubed.geo.GeometryStream
+import kubed.geo.MultiplexStream
+import kubed.geo.Position
+import kubed.math.EPSILON
+
+fun albersUsaPr() = albersUsaPr {}
+fun albersUsaPr(init: AlbersUsaPr.() -> Unit) = AlbersUsaPr().apply {
+    scale = 1070.0
+    init()
+}
+
+open class AlbersUsaPr : StreamCacheProjection() {
+    private val lower48 = albers()
+    lateinit var lower48Point: GeometryStream
+
+    private val alaska = conicEqualArea {
+        rotateX = 154.0
+        center = Position(-2.0, 58.5)
+        parallels = doubleArrayOf(55.0, 65.0)
+        scale = lower48.scale * .35
+    }
+    lateinit var alaskaPoint: GeometryStream
+
+    private val hawaii = conicEqualArea {
+        rotateX = 157.0
+        center = Position(-3.0, 19.9)
+        parallels = doubleArrayOf(8.0, 18.0)
+        scale = lower48.scale
+    }
+    lateinit var hawaiiPoint: GeometryStream
+
+    private val puertoRico = conicEqualArea {
+        rotateX = 157.0
+        center = Position(-3.0, 19.9)
+        parallels = doubleArrayOf(8.0, 18.0)
+        scale = lower48.scale
+    }
+    lateinit var puertoRicoPoint: GeometryStream
+
+    override var precision: Double
+        get() = lower48.precision
+        set(value) {
+            lower48.precision = value
+            alaska.precision = value
+            hawaii.precision = value
+            puertoRico.precision = value
+        }
+
+    override var scale: Double
+        get() = lower48.scale
+        set(k) {
+            lower48.scale = k
+            alaska.scale = k * .35
+            hawaii.scale = k
+            puertoRico.scale = k
+            update()
+        }
+
+    override var translateX: Double
+        get() = lower48.translateX
+        set(x) {
+            lower48.translateX = x
+            update()
+        }
+
+    override var translateY: Double
+        get() = lower48.translateY
+        set(y) {
+            lower48.translateY = y
+            update()
+        }
+
+    private var point: DoubleArray? = null
+    private val pointStream = object : GeometryStream {
+        override fun point(x: Double, y: Double, z: Double) {
+            point = doubleArrayOf(x, y)
+        }
+    }
+
+    init {
+        update()
+    }
+
+    private fun update() {
+        val k = scale
+        val x = translateX
+        val y = translateY
+
+        with(lower48) {
+            clipExtent = Rectangle2D(x - 0.455 * k, y - 0.238 * k, 0.91 * k, 0.476 * k)
+            lower48Point = stream(pointStream)
+        }
+
+        with(alaska) {
+            translateX = x - 0.307 * k
+            translateY = y + 0.201 * k
+            clipExtent = Rectangle2D(x - 0.425 * k + EPSILON, y + 0.120 * k + EPSILON,
+                    0.211 * k - 2 * EPSILON, 0.114 * k - 2 * EPSILON)
+            alaskaPoint = stream(pointStream)
+        }
+
+        with(hawaii) {
+            translateX = x - 0.205 * k
+            translateY = y + 0.212 * k
+            clipExtent = Rectangle2D(x - 0.214 * k + EPSILON, y + 0.166 * k + EPSILON,
+                    0.099 * k - 2 * EPSILON, 0.06 * k - 2 * EPSILON)
+            hawaiiPoint = stream(pointStream)
+        }
+
+        with(puertoRico) {
+            translateX = x + .350 * k
+            translateY = y + .224 * k
+            clipExtent = Rectangle2D(x + .320 * k, y + .204 * k, 0.06 * k, 0.03 * k)
+            puertoRicoPoint = stream(pointStream)
+        }
+
+        reset()
+        invalidate()
+    }
+
+    override fun invoke(point: DoubleArray): DoubleArray {
+        val x = point[0]
+        val y = point[1]
+
+        this.point = null
+
+        lower48Point.point(x, y, 0.0)
+        val lp = this.point
+        if(lp != null) return lp
+
+        alaskaPoint.point(x, y, 0.0)
+        val ap = this.point
+        if(ap != null) return ap
+
+        hawaiiPoint.point(x, y, 0.0)
+        val hp = this.point
+        if(hp != null) return hp
+
+        puertoRicoPoint.point(x, y, 0.0)
+        val prp = this.point
+        if(prp != null) return prp
+
+        throw Exception("Bug in AlbersUsaPr.invoke")
+    }
+
+    override fun invert(coordinates: DoubleArray): DoubleArray {
+        val x = (coordinates[0] - translateX) / scale
+        val y = (coordinates[1] - translateY) / scale
+
+        val p = when {
+            y in 0.120..0.234 && x in -0.425..-0.214 -> alaska
+            y in 0.166..0.234 && x in -0.214..-0.115 -> hawaii
+            y in 0.204..0.234 && x in 0.320..0.380 -> puertoRico
+            else -> lower48
+        }
+
+        return p.invert(coordinates)
+    }
+
+    override fun stream(forStream: GeometryStream): GeometryStream {
+        var stream = getCachedStream(forStream)
+        if(stream == null) {
+            stream = MultiplexStream(listOf(lower48.stream(forStream), alaska.stream(forStream), hawaii.stream(forStream)))
+            cache(forStream, stream)
+        }
+
+        return stream
+    }
+}
